c9e9059b70b8f1b652b524e64429925d50394d84
PatrykMaternicki <patryk.maternicki@gmail.com>
2017-11-08 04:15:28 +0100

    Address testing entity (Complete)

--- src/test/java/com/aviaSoftware/ePlatnik/ePlatnikWebService/domain/entityTest/PersonInfoTest/AddressTest.java
+++ src/test/java/com/aviaSoftware/ePlatnik/ePlatnikWebService/domain/entityTest/PersonInfoTest/AddressTest.java
@@ -12,56 +12,64 @@ import javax.persistence.Persistence;
 import static org.hamcrest.CoreMatchers.*;
 public class AddressTest  {
 
+    private Address entity;
+    private EntityManager em;
 	private Address address;
 	@Before 
 	public void Test(){
-		
+        em = Persistence.createEntityManagerFactory("testPU").createEntityManager();
 		address = new Address();
 		address.setCountry("Poland");
 		address.setCity("Gdansk");
 		address.setFlatNumber(1);
 		address.setPostalCode("83-010");
 		address.setStreet("ul.nowozenska2");
-		
+        em.getTransaction().begin();
+        em.persist(address);
+        em.getTransaction().commit();
+        entity = em.find(Address.class, address.getId());
+        em.close();
 	}
 	
 	@Test
 	public void should_be_country_is_string(){
-		EntityManager em = Persistence.createEntityManagerFactory("testPU").createEntityManager();
-		Address address1 = new Address();
-		address1.setCountry("Poland");
-		address1.setCity("Gdansk");
-		address1.setFlatNumber(1);
-		address1.setPostalCode("83-010");
-		address1.setStreet("ul.nowozenska2");
-		em.getTransaction().begin();
-		em.persist(address1);
-		em.getTransaction().commit();
-		//Address foundAddress = em.find(Address.class, address.getId());
-		//assertEquals("Field country in Address is not string",foundAddress.getCountry(),is(String.class));
-		em.close();
+		assertThat("Field country in Address is not string",address.getCountry(),is(String.class));
 	}
-	/*
+	@Test
+    public void testPU_should_be_return_country(){
+	    assertEquals("Entity address column country is not equal country in Address object",address.getCountry(),entity.getCountry());
+    }
 	@Test
 	public void should_be_city_is_string(){
 		assertThat("Field City in Address is not string",address.getCity(),is(String.class));
-
 	}
+	@Test
+    public void testPU_should_be_return_city(){
+	    assertEquals("Entity address column city is not equal city on object",address.getCity(),entity.getCity());
+    }
 	@Test
 	public void should_be_flatNumber_is_Integer(){
 		assertThat("Field flatNumber in Address is not integer",address.getFlatNumber(),is(Integer.class));
-
 	}
+	@Test
+    public void testPU_should_be_return_flatNumber(){
+        assertEquals("Entity address column flatNumber is not equal flatNumber in Address object",address.getFlatNumber(),entity.getFlatNumber());
+    }
 	@Test
 	public void should_be_postalCode_is_istring(){
 		assertThat("Field postalCode in Address is not string",address.getPostalCode(),is(String.class));
-
 	}
+	@Test
+	public void testPU_should_be_return_postalCode(){
+        assertEquals("Entity address column postalCode is not equal postalCode in Address object",address.getPostalCode(),entity.getPostalCode());
+    }
 	@Test
 	public void should_be_street_is_Integer(){
 		assertThat("Field street in Address is not string",address.getStreet(),is(String.class));
-	
 	}
-	*/
-	
+	@Test
+    public void testPU_should_be_return_street(){
+        assertEquals("Entity address column street is not equal street in Address object",address.getStreet(),entity.getStreet());
+    }
+
 }
